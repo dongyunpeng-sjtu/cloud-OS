42795536b55fe1c972a244c54348d5cb1b5d01cb
dongyunpeng <dongyunpeng@sjtu.edu.cn>
2023-03-05 19:38:12 +0800

    lab1

--- rdt/rdt_receiver.cc
+++ rdt/rdt_receiver.cc
@@ -8,6 +8,8 @@
  *       
  *       |<-  1 byte  ->|<-             the rest            ->|
  *       | payload size |<-             payload             ->|
+ *       |<-  4 byte  ->|<-  2 byte  ->|<-  2 byte  ->|
+ *       | sequence_num |  ack         |  checksum    |
  *
  *       The first byte of each packet indicates the size of the payload
  *       (excluding this single-byte header)
@@ -17,11 +19,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <iostream>
+#include <unordered_map>
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
+#include <string>
 
 
+const int window = 10;
+const int sizeof_sequence_num = 4;
+const int sizeof_ack = 2;
+const int sizeof_checksum = 2;
+const int sizeof_payload_size=1;
+/* 1-byte header indicating the size of the payload */
+const int header_size = sizeof_sequence_num + sizeof_checksum + sizeof_payload_size;
+/* position pointer of each */
+const int pointer_sequence_num = 0;
+const int pointer_check = 126;
+const int pointer_payload_size = sizeof_sequence_num;
+const int pointer_ack = sizeof_sequence_num ;
+const int pointer_payload = sizeof_sequence_num  + sizeof_payload_size;
+const double timeout = 0.3;
+unsigned int receiver_next_seq_num = 0;
+
+std::unordered_map<unsigned int, packet*> receiver_buffer;
 /* receiver initialization, called once at the very beginning */
 void Receiver_Init()
 {
@@ -37,29 +58,136 @@ void Receiver_Final()
     fprintf(stdout, "At %.2fs: receiver finalizing ...\n", GetSimulationTime());
 }
 
+uint16_t checksum2(struct packet *pkt)
+{
+    unsigned long checksum = 0; 
+    
+    for (int i = 0; i < 126; i += 2) {
+        checksum += *(short *)(&(pkt->data[i]));
+    }
+    while (checksum >> 16) {
+        checksum = (checksum >> 16) + (checksum & 0xffff);
+    }
+    return ~checksum;
+}
+uint16_t checksum4(struct packet *pkt)
+{
+    unsigned long checksum = 0; 
+    
+    for (int i = 0; i < 6; i += 2) {
+        checksum += *(short *)(&(pkt->data[i]));
+    }
+    while (checksum >> 16) {
+        checksum = (checksum >> 16) + (checksum & 0xffff);
+    }
+    return ~checksum;
+}
+
+void send_ack(unsigned int ack_num, short ack) {
+    struct packet* ack_pkt = (struct packet*) malloc(sizeof(struct packet));
+
+    memcpy(ack_pkt->data, &ack_num, sizeof_sequence_num);
+    memcpy(ack_pkt->data + 4, &ack, sizeof_ack);
+
+    uint16_t che=checksum4(ack_pkt);
+    memcpy(ack_pkt->data + 6, &che, sizeof_checksum);
+    Receiver_ToLowerLayer(ack_pkt); 
+};
+
+
 /* event handler, called when a packet is passed from the lower layer at the 
    receiver */
 void Receiver_FromLowerLayer(struct packet *pkt)
 {
-    /* 1-byte header indicating the size of the payload */
-    int header_size = 1;
 
-    /* construct a message and deliver to the upper layer */
-    struct message *msg = (struct message*) malloc(sizeof(struct message));
-    ASSERT(msg!=NULL);
+    unsigned int seq_num = 0;
+    short payload_size = 0;
+    uint16_t che = 0;
+    memcpy(&seq_num, pkt->data, sizeof_sequence_num);
+    memcpy(&payload_size, pkt->data + 4, sizeof_payload_size);
+    memcpy(&che, pkt->data + 126, sizeof_checksum);
+    // /* 1-byte header indicating the size of the payload */
+    // int header_size = 1;
 
-    msg->size = pkt->data[0];
+    // /* construct a message and deliver to the upper layer */
+    // struct message *msg = (struct message*) malloc(sizeof(struct message));
+    //这里对于坏包的情况做检查，防止段错误
+    if (payload_size < 0) payload_size = 0;
+    if (payload_size > RDT_PKTSIZE - header_size) payload_size = RDT_PKTSIZE - header_size;
+    std::string data(pkt->data + pointer_payload, pkt->data + pointer_payload + payload_size);
+    uint16_t check=checksum2(pkt);
+    //std::cout<<che<<"  "<<check<<"       "<<seq_num<<"  "<<receiver_next_seq_num<<std::endl;
+    if(che==check  && seq_num == receiver_next_seq_num){
+      //fprintf(stdout, "At %.2fs: aaaaa ...\n", GetSimulationTime());
+      //fprintf(stdout, "At %.2fs: bbbbb ...\n", GetSimulationTime());
+        receiver_buffer[seq_num] = pkt;
 
-    /* sanity check in case the packet is corrupted */
-    if (msg->size<0) msg->size=0;
-    if (msg->size>RDT_PKTSIZE-header_size) msg->size=RDT_PKTSIZE-header_size;
+        //这里是
+        while (receiver_buffer.find(receiver_next_seq_num) != receiver_buffer.end()) {          
+        packet* more_pkt = receiver_buffer[receiver_next_seq_num]; 
+        ASSERT(more_pkt != NULL);
+        unsigned int more_seq_num = 0;
+        memcpy(&more_seq_num, more_pkt->data + pointer_sequence_num, sizeof_sequence_num); 
+        ASSERT(more_seq_num == receiver_next_seq_num);
+        short more_che = 0;
+        memcpy(&more_che, more_pkt->data + pointer_check, sizeof_checksum);
+        char more_payload_size = 0;
+        memcpy(&more_payload_size, more_pkt->data + pointer_payload_size, sizeof_payload_size); 
 
-    msg->data = (char*) malloc(msg->size);
-    ASSERT(msg->data!=NULL);
-    memcpy(msg->data, pkt->data+header_size, msg->size);
-    Receiver_ToUpperLayer(msg);
+        struct message *more_msg = (struct message*) malloc(sizeof(struct message));
+        ASSERT(more_msg != NULL);
+        more_msg->size = more_payload_size;
 
-    /* don't forget to free the space */
-    if (msg->data!=NULL) free(msg->data);
-    if (msg!=NULL) free(msg);
+        more_msg->data = (char*) malloc(more_msg->size);
+        ASSERT(more_msg->data != NULL);
+        memcpy(more_msg->data, more_pkt->data + pointer_payload, more_payload_size);
+        Receiver_ToUpperLayer(more_msg); // printf("Receiver: send packet[%d] to upper layer\n", more_seq_num);
+        /* don't forget to free the space */
+        if (more_msg->data != NULL) free(more_msg->data);
+        if (more_msg != NULL) free(more_msg);
+
+        receiver_next_seq_num++;
+      };
+
+    /* send back ACK */
+    send_ack(receiver_next_seq_num - 1, 1);
+    
+    } 
+    else if (che == check && receiver_next_seq_num < seq_num) { // buffer it
+      packet* buffer_pkt = (struct packet*) malloc(sizeof(struct packet));
+      ASSERT(buffer_pkt);
+      memcpy(buffer_pkt->data, pkt->data, sizeof(pkt->data));
+      receiver_buffer[seq_num] = buffer_pkt;
+
+      /* send back NACK */
+      send_ack(receiver_next_seq_num, 0);
+      //fprintf(stdout, "At %.2fs: bbbbb ...\n", GetSimulationTime());
+    }
+    else send_ack(receiver_next_seq_num - 1, 1);
+    
+
+
+
+
+
+
+
+
+
+
+
+    // msg->size = pkt->data[0];
+
+    // /* sanity check in case the packet is corrupted */
+    // if (msg->size<0) msg->size=0;
+    // if (msg->size>RDT_PKTSIZE-header_size) msg->size=RDT_PKTSIZE-header_size;
+
+    // msg->data = (char*) malloc(msg->size);
+    // ASSERT(msg->data!=NULL);
+    // memcpy(msg->data, pkt->data+header_size, msg->size);
+    // Receiver_ToUpperLayer(msg);
+
+    // /* don't forget to free the space */
+    // if (msg->data!=NULL) free(msg->data);
+    // if (msg!=NULL) free(msg);
 }
--- rdt/rdt_sender.cc
+++ rdt/rdt_sender.cc
@@ -6,26 +6,44 @@
  *       situations.  In this implementation, the packet format is laid out as 
  *       the following:
  *       
- *       |<-  1 byte  ->|<-             the rest            ->|
- *       | payload size |<-             payload             ->|
+ *       |<-  4 byte  ->|<-  1 byte  ->|<-   121 byte            ->|<-  2 byte  ->|
+ *       | sequence_num | payload size |<-   payload             ->| checksum     |
  *
- *       The first byte of each packet indicates the size of the payload
- *       (excluding this single-byte header)
+ *       The initial 4 bytes are seq_num
+ *       Then is payload size of 1 byte
+ *       Then is payload
+ *       Finally is checksum 
  */
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <deque>
 #include "rdt_struct.h"
 #include "rdt_sender.h"
+#include <string>
 
-
+const int window =10;
+const int sizeof_sequence_num = 4;
+const int sizeof_payload_size = 1;
+const int  sizeof_checksum  = 2;
+const int  sizeof_ack = 2;
+const int  payload_size_offset =4;
+const int  payload_offset=5;
+unsigned int sender_base = 0;
+unsigned int sender_next_seq_num = 0;
+const double timeout = 0.3;
+const int ack_offset=4;
+const int che_offset=6;
+std::deque<packet> sender_window = std::deque<packet>();
 /* sender initialization, called once at the very beginning */
 void Sender_Init()
 {
     fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
+    sender_base = 0;
+    sender_next_seq_num = 0;
+    sender_window.clear();
 }
 
 /* sender finalization, called once at the very end.
@@ -37,54 +55,137 @@ void Sender_Final()
     fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
 }
 
+uint16_t checksum(struct packet *pkt)
+{
+    unsigned long checksum = 0; 
+    
+    for (int i = 0; i < 126; i += 2) {
+        checksum += *(short *)(&(pkt->data[i]));
+    }
+    while (checksum >> 16) {
+        checksum = (checksum >> 16) + (checksum & 0xffff);
+    }
+    return ~checksum;
+}
+uint16_t checksum3(struct packet *pkt)
+{
+    unsigned long checksum = 0; 
+    
+    for (int i = 0; i < 6; i += 2) {
+        checksum += *(short *)(&(pkt->data[i]));
+    }
+    while (checksum >> 16) {
+        checksum = (checksum >> 16) + (checksum & 0xffff);
+    }
+    return ~checksum;
+}
+
+
+
 /* event handler, called when a message is passed from the upper layer at the 
    sender */
 void Sender_FromUpperLayer(struct message *msg)
 {
     /* 1-byte header indicating the size of the payload */
-    int header_size = 1;
+    int header_size = sizeof_payload_size+sizeof_sequence_num+ sizeof_checksum ;
 
     /* maximum payload size */
-    int maxpayload_size = RDT_PKTSIZE - header_size;
+    int maxpayload_size = 121;
 
     /* split the message if it is too big */
 
     /* reuse the same packet data structure */
     packet pkt;
 
-    /* the cursor always points to the first unsent byte in the message */
+    /* the cursor always points to the first unsent byte in the message *///这里是指message中因为split没有寄出的byte
     int cursor = 0;
+    //fprintf(stdout, "At %.2fs: aaaaa ...\n", GetSimulationTime());
+    //如果装不下去，就一直循环拆分
+    while (msg->size > cursor)
+    {
+        char payload_size = std::min(maxpayload_size, msg->size - cursor);
+        // void *memcpy(void *str1, const void *str2, size_t n) 从存储区 str2 复制 n 个字节到存储区 str1
+        //这一步是复制payloadsize,是否有必要？
+        memcpy(pkt.data + payload_size_offset, &payload_size, 1);
+        //这一步复制整个payload
+        memcpy(pkt.data + payload_offset, msg->data + cursor, payload_size);
 
-    while (msg->size-cursor > maxpayload_size) {
-	/* fill in the packet */
-	pkt.data[0] = maxpayload_size;
-	memcpy(pkt.data+header_size, msg->data+cursor, maxpayload_size);
+        unsigned int seq_num = sender_window.size();
+        memcpy(pkt.data , &seq_num, sizeof_sequence_num);
 
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(&pkt);
+        
 
-	/* move the cursor */
-	cursor += maxpayload_size;
+
+        uint16_t check= checksum(&pkt);
+        memcpy(pkt.data + 126, &check, sizeof_checksum);
+
+        /* throw it into sender_window */
+        sender_window.push_back(pkt);
+
+        printf("Sender: packet[%d] size %d\n", seq_num, payload_size);
+
+        /* move the cursor */
+        cursor += maxpayload_size;
     }
+    //fprintf(stdout, "At %.2fs: bbbbb ...\n", GetSimulationTime());
+    /* send first N packet */
+    while (sender_next_seq_num < sender_base + window && sender_next_seq_num < sender_window.size()) {
+        Sender_ToLowerLayer(&sender_window[sender_next_seq_num]); 
+        printf("Sender: send packet[%d]\n", sender_next_seq_num);
+        sender_next_seq_num++;
+    };
 
-    /* send out the last packet */
-    if (msg->size > cursor) {
-	/* fill in the packet */
-	pkt.data[0] = msg->size-cursor;
-	memcpy(pkt.data+header_size, msg->data+cursor, pkt.data[0]);
+    Sender_StartTimer(timeout);
 
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(&pkt);
-    }
 }
 
 /* event handler, called when a packet is passed from the lower layer at the 
    sender */
+// |<-  4 byte  ->|<-  2 byte  ->|<-  2 byte  ->|
+// | sequence_num |    ack/nack  |  checksum    |
+//这里是receiver在回应sender,表示ack或non-ack
 void Sender_FromLowerLayer(struct packet *pkt)
 {
+    ASSERT(pkt != NULL);
+    Sender_StartTimer(timeout);
+
+    unsigned int ack_num = 0;
+    short ack = -1;
+    uint16_t che = 0;
+
+    memcpy(&ack_num, pkt->data, sizeof_sequence_num);
+    memcpy(&ack, pkt->data + ack_offset, sizeof_ack);
+    memcpy(&che, pkt->data + che_offset, sizeof_checksum);
+
+    if(checksum3(pkt)==che){
+        if(ack==1){//ack
+            sender_base = ack_num + 1;
+            while (sender_next_seq_num < sender_base + window && sender_next_seq_num < sender_window.size()) {
+                Sender_ToLowerLayer(&sender_window[sender_next_seq_num]); 
+                 printf("Sender success: send packet[%d]\n", sender_next_seq_num);
+                sender_next_seq_num++;
+            };
+        }else if (ack == 0) { // nack message
+        unsigned int nack_num = ack_num;
+        Sender_ToLowerLayer(&sender_window[nack_num]); //重传
+    }
+    }
+
+    if (sender_base == sender_next_seq_num) {
+        Sender_StopTimer();
+    }
 }
 
 /* event handler, called when the timer expires */
 void Sender_Timeout()
 {
+
+    Sender_StartTimer(timeout);
+
+    /* resend all packet in the window */
+    for (unsigned int i = sender_base; i < sender_next_seq_num; i++) {
+        Sender_ToLowerLayer(&sender_window[i]); 
+    };
+
 }
+
